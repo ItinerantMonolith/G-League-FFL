dc58d7f44a9579fcb39172a2b2da828ff4cc7c54
ItinerantMonolith <ItinerantMonolith@gmail.com>
2020-11-15 21:22:00 -0500

    fixed addPlayer so that players on rosters don't show up in pick lists.

--- controllers/PlayerController.js
+++ controllers/PlayerController.js
@@ -1,5 +1,5 @@
 const Team = require('../db/models/Team')
-const { Player, NFLTeam } = require('../db/schema')
+const { Player, League, Roster } = require('../db/schema')
 
 // return a single player based on ID
 const GetPlayer = async (req, resp) => {
@@ -7,16 +7,24 @@ const GetPlayer = async (req, resp) => {
    resp.send(player)
 }
 
-// return all [+== available] players based on team and position
+// return all available players based on team and position
 const GetPlayers = async (req, resp) => {
-   // need to get the team first
-   
+   // get all players who are currently rostered.
+   // first we need to know what the current week is
+   const league = await League.findOne()
+   // now get all rosters for that week
+   const rosters = await Roster.find( { week: league.currentWeek } )
+
+   // now build an array of all the objectID's in those rosters
+   let playerList = []
+   rosters.forEach ( e => playerList = [...playerList, ...e.players] )
 
    // const team = await NFLTeam.findById ( req.params.nflTeam )
    const pos = req.params.position ? req.params.position.toUpperCase() : 'QB'
    const players = await Player.find({
       team: req.params.nflTeam,
       position: pos,
+      _id: { $nin: playerList}
    }).select('name')
    resp.send(players)
 }
